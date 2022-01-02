0dbf87d3b6cb44b733ff2d063f5b1cc8c125d5a7
NickDCrisp <75099056+NickDCrisp@users.noreply.github.com>
2022-01-02 18:00:46 +0000

    Introduced int16 version of ReadC
    Moved my content which I had developed in an older LadyAda version in to the most recent library from AdaFruit.  Some comment updates and added a version of the read function readC which returns an integer temperature in C.  This throws away some resolution but improves run time.

--- max6675.cpp
+++ max6675.cpp
@@ -1,7 +1,10 @@
 // this library is public domain. enjoy!
 // https://learn.adafruit.com/thermocouple/
+// NDC added fixed precision routine returning integer temperature in Celsius to be compatible with our Sensor library.
+// My original based on an earlier version at www.ladyada.net/learn/sensors/thermocouple
 
 #include "max6675.h"
+// Note references to delay are available without needing explicit include statement.
 
 /**************************************************************************/
 /*!
@@ -21,7 +24,11 @@ MAX6675::MAX6675(int8_t SCLK, int8_t CS, int8_t MISO) {
   pinMode(sclk, OUTPUT);
   pinMode(miso, INPUT);
 
+  // de-select
   digitalWrite(cs, HIGH);
+  // NDC: initial value is high we pulse low to read in spiread() routine
+  digitalWrite(sclk, HIGH);
+
 }
 
 /**************************************************************************/
@@ -35,6 +42,7 @@ float MAX6675::readCelsius(void) {
   uint16_t v;
 
   digitalWrite(cs, LOW);
+  // shorter delay than previous which improves read time.
   delayMicroseconds(10);
 
   v = spiread();
@@ -56,12 +64,52 @@ float MAX6675::readCelsius(void) {
 
 /**************************************************************************/
 /*!
+    @brief  Read the Celsius temperature
+    @returns integer Temperature in C
+	or fixed very negative constant value OPENCIRCUIT on failure!
+*/
+// MSB bit 14, LSB bit 3.  T/C input status bit 2
+// NDC> added integer version of the input, resolution one degree
+// returns 0...1023 in-range, or -300 if not connected.
+int16_t MAX6675::readC(void) {
+
+  uint16_t v;
+
+  digitalWrite(cs, LOW);
+  // shorter delay than previous which improves read time.
+  delayMicroseconds(10);
+
+  v = spiread();
+  v <<= 8;
+  v |= spiread();
+
+  digitalWrite(cs, HIGH);
+
+  if (v & 0x4) {
+    // uh oh, no thermocouple attached!
+    return OPENCIRCUIT;
+	// Note NAN is not valid for integer type
+  }
+
+  // remove status bits and the 0.25 resolution leaving data in C
+  v >>= 5;
+
+  return (int16_t) v;
+}
+
+/**************************************************************************/
+/*!
     @brief  Read the Fahenheit temperature
     @returns Temperature in F or NAN on failure!
 */
 /**************************************************************************/
-float MAX6675::readFahrenheit(void) { return readCelsius() * 9.0 / 5.0 + 32; }
+float MAX6675::readFahrenheit(void) {
+	return readCelsius() * 9.0 / 5.0 + 32;
+}
 
+
+// private function for SPI read on digital pins
+// requires CS to be activated outside this routine.  It controls SCLK
 byte MAX6675::spiread(void) {
   int i;
   byte d = 0;
@@ -70,7 +118,7 @@ byte MAX6675::spiread(void) {
     digitalWrite(sclk, LOW);
     delayMicroseconds(10);
     if (digitalRead(miso)) {
-      // set the bit to 0 no matter what
+      // Bit will remain 0 if condition is false
       d |= (1 << i);
     }
 
--- max6675.h
+++ max6675.h
@@ -1,5 +1,10 @@
-// this library is public domain. enjoy!
-// https://learn.adafruit.com/thermocouple/
+
+/* this library is based on the AdaFruit public domain. enjoy!
+ * https://learn.adafruit.com/thermocouple/
+ * I've updated this based on work I did no a previous public domain library at www.ladyada.net/learn/sensors/thermocouple
+ * This will interface over digital pins due to low data rate no need for HW SPI.
+ * NDC added an integer version to avoid floating point operations and to work with our Sensor class
+ */
 
 #ifndef ADAFRUIT_MAX6675_H
 #define ADAFRUIT_MAX6675_H
@@ -15,12 +20,16 @@ class MAX6675 {
 public:
   MAX6675(int8_t SCLK, int8_t CS, int8_t MISO);
 
+  /* Does in-line query of SPI sensor and returns value converting to Celsius as float */
   float readCelsius(void);
+  /* Does in-line query of SPI sensor and returns value converting to Fahrenheit as float */
   float readFahrenheit(void);
+  /* Does in-line query of SPI sensor and returns value converting to Celsius as 16-bit signed integer
+   * Returns fixed value on failure  */
+  int16_t readC(void);
 
-  /*!    @brief  For compatibility with older versions
-         @returns Temperature in F or NAN on failure! */
-  float readFarenheit(void) { return readFahrenheit(); }
+  // a value to return when we detect open circuit
+  const int16_t OPENCIRCUIT = -300;
 
 private:
   int8_t sclk, miso, cs;
